cfde03e58fbb36eb63097d41c554b174720d695f
jinsang <jinsang@nhndata.com>
2022-07-28 16:51:32 +0900

    - List 컴포넌트, tahoe_peaks 분리 - faker 설치

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import "./App.css"
 import React from "react"
-import { GitHubUser } from "./components"
-
-const tahoe_peaks = [
-  { name: "Freel Peak", elevation: 10891 },
-  { name: "Monument Peak", elevation: 10067 },
-  { name: "Pyramid Peak", elevation: 9983 },
-  { name: "Mt. Tallac", elevation: 9735 },
-]
-
-function List({ data = [], renderEmpty, renderItem }) {
-  if (!data.length) return renderEmpty
-  return <ul>{data.map(renderItem)}</ul>
-}
+import { tahoe_peaks, List } from "./components"
 
 function App() {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import AddColorForm from "./AddColorForm"
 import ColorList from "./ColorList"
 import GitHubUser from "./GitHubUser"
+import { tahoe_peaks, List } from "./List"
 
-export { AddColorForm, ColorList, GitHubUser }
+export { AddColorForm, ColorList, GitHubUser, tahoe_peaks, List }
